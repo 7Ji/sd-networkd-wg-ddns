4409179d7b7c1dcacc2866adbdee6102dae69fbb
Guoxin Pu <pugokushin@gmail.com>
2024-05-15 13:15:22 +0800

    finish networkd config parsing

--- sd-networkd-wg-ddns.c
+++ sd-networkd-wg-ddns.c
@@ -1,5 +1,6 @@
 // WIP, DO NOT USE
 
+#include <limits.h>
 #include <linux/limits.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,30 +20,47 @@
     println_with_prefix_and_source("ERROR", format, ##arg)
 #define println_error_with_errno(format, arg...) \
     println_error(format", errno: %d, error: %s\n", ##arg, errno, strerror(errno))
-#define SIZE_BUFFER 0x100000
 
+#define min2(A, B) (A > B ? B : A)
+
+#define SIZE_BUFFER 0x100000
 #define PATH_CONFIGS "/etc/systemd/network"
-// #define LEN_PATH_CONFIGS sizeof(PATH_CONFIGS) - 1
-// #define CONFIG_NAME_MAXLEN PATH_MAX - sizeof(PATH_CONFIGS) + 1
 #define NETDEV_STEM_MAX NAME_MAX - 6 // including terminating '\0', NAME_MAX + 1 - 7
+#define TITLE_NETDEV "NetDev"
+#define TITLE_PEER "WireGuardPeer"
+#define KEY_NAME "Name"
+#define KEY_KIND "Kind"
+#define KEY_PUBLICKEY "PublicKey"
+#define KEY_ENDPOINT "Endpoint"
+#define VALUE_WIREGUARD "wireguard"
+#define LEN_KEY_KIND (sizeof KEY_KIND) - 1
+#define LEN_KEY_NAME (sizeof KEY_NAME) - 1
+#define LEN_VALUE_WIREGUARD (sizeof VALUE_WIREGUARD) -1
+#define LEN_KEY_PUBLICKEY (sizeof KEY_PUBLICKEY) - 1
+#define LEN_KEY_ENDPOINT (sizeof KEY_ENDPOINT) - 1
 
 struct peer {
     char public_key[45]; // len = 44
-    char endpoint[48]; // len = 47 = 2([]) + 8x4 + 7 (IPv6) + 1 + 5 (port)
+    char endpoint_host[256]; // len = 255 (max length of domain name)
+    unsigned short endpoint_port;
+    unsigned short len_public_key;
+    unsigned short len_endpoint_host;
     unsigned long latest_handshake;
 };
 
 struct netdev {
     char name[16]; // len = 15
+    unsigned short len_name;
     struct peer *peers;
     unsigned short peers_count;
     unsigned short peers_allocated;
 };
 
-enum read_status {
-    none,
-    section_title,
-    section_line
+enum parse_status {
+    parse_status_none,
+    parse_status_netdev_section,
+    parse_status_peer_section,
+    parse_status_other_section
 };
 
 int parse_netdev_buffer(
@@ -50,8 +68,11 @@ int parse_netdev_buffer(
     char const *const restrict buffer,
     size_t size_buffer
 ) {
-    struct peer* peers_buffer;
-    size_t line_start, line_end, stripped_start, stripped_end, len_stripped;
+    struct peer* peers_buffer, *peer;
+    size_t line_start, line_end, stripped_start, stripped_end, len_stripped, key_end, len_key, value_start, len_value, host_end, host_start, len_host, len_port, port_start;
+    enum parse_status parse_status;
+    char const *key, *value;
+    char buffer_port[6];
     int r;
 
     netdev->peers = malloc(sizeof *netdev->peers * 0x10);
@@ -62,24 +83,153 @@ int parse_netdev_buffer(
     netdev->peers_allocated = 0x10;
     netdev->peers_count = 0;
     netdev->name[0] = '\0';
+    netdev->len_name = 0;
 
+    parse_status = parse_status_none;
     for (line_start = 0; line_start < size_buffer; line_start = line_end + 1) {
         for (line_end = line_start; line_end < size_buffer; ++line_end) {
             if (buffer[line_end] == '\n' || buffer[line_end] == '\0') break;
         }
-        if (line_end <= line_start + 1) continue;
+        if (line_end < line_start + 1) continue;
         for (stripped_start = line_start; stripped_start < line_end && (buffer[stripped_start] == ' ' || buffer[stripped_start] == '\t'); ++stripped_start);
         if (buffer[stripped_start] == '#') continue;
         for (stripped_end = line_end - 1; stripped_end > stripped_start && (buffer[stripped_end] == ' ' || buffer[stripped_end] == '\t'); --stripped_end);
+        if (stripped_end < stripped_start) continue;
         ++stripped_end;
         len_stripped = stripped_end - stripped_start;
-        if (!len_stripped) continue;
-        // This is currently the stripped line
         // printf("Line (%lu): '", len_stripped);
         // for (size_t i = stripped_start; i < stripped_end; ++i) {
         //     putchar(buffer[i]);
         // }
         // printf("'\n");
+        // Section title
+        key = buffer + stripped_start;
+        if (buffer[stripped_start] == '[') {
+            if (buffer[stripped_end - 1] != ']') {
+                println_error("Sectition title not ended");
+                r = -1;
+                goto free_peers;
+            }
+            ++key;
+            if (len_stripped == sizeof TITLE_NETDEV + 1 &&
+                !strncmp(key, TITLE_NETDEV, sizeof TITLE_NETDEV - 1)) 
+            {
+                parse_status = parse_status_netdev_section;
+            } else if (len_stripped == sizeof TITLE_PEER + 1 &&
+                !strncmp(key, TITLE_PEER, sizeof TITLE_PEER - 1)) 
+            {
+                ++netdev->peers_count;
+                if (netdev->peers_count > netdev->peers_allocated) {
+                    while (netdev->peers_count > netdev->peers_allocated) {
+                        if (netdev->peers_allocated == USHRT_MAX) {
+                            println_error("Cannot allocate more memory for peers");
+                            r = -1;
+                            goto free_peers;
+                        } else if (netdev->peers_allocated > USHRT_MAX / 2) {
+                            netdev->peers_allocated = USHRT_MAX;
+                        } else {
+                            netdev->peers_allocated *= 2;
+                        }
+                    }
+                    peers_buffer = realloc(netdev->peers, sizeof *netdev->peers * netdev->peers_count);
+                    if (!peers_buffer) {
+                        println_error_with_errno("Failed to reallocat memory for peers");
+                        r = -1;
+                        goto free_peers;
+                    }
+                    netdev->peers = peers_buffer;
+                }
+                peer = netdev->peers + netdev->peers_count - 1;
+                peer->latest_handshake = 0;
+                peer->endpoint_host[0] = '\0';
+                peer->len_endpoint_host = 0;
+                peer->endpoint_port = 0;
+                peer->public_key[0] = '\0';
+                peer->len_public_key = 0;
+                parse_status = parse_status_peer_section;
+            } else {
+                parse_status = parse_status_other_section;
+            }
+            continue;
+        }
+        // Section content
+        switch (parse_status) {
+        case parse_status_none:
+        case parse_status_other_section:
+            continue;
+        default:
+            break;
+        }
+        for (key_end = stripped_start; key_end < stripped_end; ++key_end) {
+            if (buffer[key_end] == '=') break;
+        }
+        if (key_end <= stripped_start) continue;
+        len_key = key_end - stripped_start;
+        value_start = key_end + 1;
+        len_value = stripped_end - value_start;
+        value = buffer + value_start;
+        switch (parse_status) {
+        case parse_status_netdev_section:
+            if (len_key == LEN_KEY_KIND) {
+                if (!strncmp(key, KEY_KIND, LEN_KEY_KIND)) {
+                    if (strncmp(value, VALUE_WIREGUARD, LEN_VALUE_WIREGUARD)) {
+                        println_error("Netdev kind is not '"VALUE_WIREGUARD"'");
+                        r = -1;
+                        goto free_peers;
+                    }
+                } else if (!strncmp(key, KEY_NAME, LEN_KEY_NAME)) {
+                    netdev->len_name = min2(len_value, sizeof netdev->name - 1);
+                    memcpy(netdev->name, value, netdev->len_name);
+                    netdev->name[netdev->len_name] = '\0';
+                }
+            }
+            break;
+        case parse_status_peer_section:
+            peer = netdev->peers + netdev->peers_count - 1;
+            switch (len_key) {
+            case LEN_KEY_PUBLICKEY:
+                if (!strncmp(key, KEY_PUBLICKEY, LEN_KEY_PUBLICKEY)) {
+                    peer->len_public_key = min2(len_value, sizeof peer->public_key - 1);
+                    memcpy(peer->public_key, value, peer->len_public_key);
+                    peer->public_key[peer->len_public_key] = '\0';
+                }
+                break;
+            case LEN_KEY_ENDPOINT:
+                if (!strncmp(key, KEY_ENDPOINT, LEN_KEY_ENDPOINT)) {
+                    for (host_end = stripped_end - 1; host_end > value_start; --host_end) {
+                        if (buffer[host_end] == ':') break;
+                    }
+                    if (host_end <= value_start) continue; // Illegal
+                    // Port
+                    port_start = host_end + 1;
+                    if (port_start >= stripped_end) continue; // Illegal
+                    len_port = stripped_end - port_start;
+                    // Host
+                    host_start = value_start;
+                    if (buffer[host_start] == '[' && buffer[host_end - 1] == ']') {
+                        ++host_start;
+                        --host_end;
+                        if (host_end <= host_start) continue; // Illegal
+                    }
+                    len_host = host_end - value_start;
+                    // Read
+                    len_port = min2(len_port, sizeof buffer_port - 1);
+                    memcpy(buffer_port, buffer + port_start, len_port);
+                    buffer_port[len_port] = '\0';
+                    if (sscanf(buffer + host_end + 1, "%hu", &peer->endpoint_port) != 1) {
+                        println_error("Endpoint port ('%s') could not be parsed", buffer_port);
+                        continue;
+                    }
+                    peer->len_endpoint_host = min2(len_host, sizeof peer->endpoint_host - 1);
+                    memcpy(peer->endpoint_host, value, peer->len_endpoint_host);
+                    peer->endpoint_host[peer->len_endpoint_host] = '\0';
+                }
+                break;
+            }
+            break;
+        default:
+            break;
+        }
     }
     if (!netdev->peers_count) {
         free(netdev->peers);
@@ -100,6 +250,22 @@ free_peers:
         if (netdev->peers) free(netdev->peers);
     }
     return r;
+}
+
+void dump_netdev(
+    struct netdev const *const restrict netdev
+) {
+    unsigned short i;
+    struct peer const *peer;
+
+    println_info("Netdev '%s':", netdev->name);
+    for (i = 0; i < netdev->peers_count; ++i) {
+        println_info(" => Peer %hu:", i);
+        peer = netdev->peers + i;
+        println_info("  -> Public Key: %s", peer->public_key);
+        println_info("  -> Host: %s", peer->endpoint_host);
+        println_info("  -> Port: %hu", peer->endpoint_port);
+    }
 }
 
 int parse_netdev_config(
@@ -153,6 +319,7 @@ int parse_netdev_config(
         r = -1;
         goto free_heap;
     }
+    dump_netdev(netdev);
     r = 0;
 free_heap:
     if (buffer_heap) free(buffer_heap);
